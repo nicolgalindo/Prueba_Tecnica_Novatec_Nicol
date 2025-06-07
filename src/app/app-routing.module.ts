0f5d12b76526514ab9750b53f55195471b116e18
Nicol Maria Alejandra Galindo Mojica <NGALINDO@bancodeoccidente.com.co>
2025-06-07 16:33:15 -0500

    Primer commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './components/user-list/user-list.component';
+
+const routes: Routes = [
+  {
+    path:'user',
+    component: UserListComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

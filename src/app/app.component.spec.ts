30b8f9068c7e02859366451d419ea855ac031426
Nicol Maria Alejandra Galindo Mojica <nicomaria21@hotmail.com>
2025-06-08 21:40:27 -0500

    Funcionalidad carrito

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'prueba360'`, () => {
+  it(`should have as title 'bank-inc'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('prueba360');
+    expect(app.title).toEqual('bank-inc');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('prueba360 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('bank-inc app is running!');
   });
 });
